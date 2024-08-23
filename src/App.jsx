4788de55c709b29a9a90c69407b80bf3a15a052b
sarthak italiya <sarthak.italiya2005@gmail.com>
2024-08-23 16:49:51 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
+import Testimonials from "./Components/Testimonials/Testimonials";
 import Title from "./Components/Title/Title";
 
 export default function App() {
@@ -14,6 +18,12 @@ export default function App() {
         <Programs />
         <About />
         <Title subTitle='Gallery' title='Campus Photo' />
+        <Campus />
+        <Title subTitle='TESTIMONIALS' title='What Student Says' />
+        <Testimonials />
+        <Title subTitle='Contact Us' title='Get in Touch' />
+        <Contact />
+        <Footer />
       </div>
     </div>
   )
